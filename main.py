9c4811194b18a8cd6d9d23f1fc9b39aefa951c1f
IngNoN <sebastian.karner@bulme.at>
2022-03-04 22:07:19 +0100

    Add Scrolllist with background, add item Scrolllist.kv, add AddItem-Button

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from kivy.app import App
-from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
-from kivy.uix.textinput import TextInput
-from kivy.uix.checkbox import CheckBox
-from kivy.lang import Builder
-from kivy import Config
+from kivy.config import Config
+from kivy.uix.gridlayout import GridLayout
 
 Config.set("graphics", "width", "270")
 Config.set("graphics", "height", "480")
+
+from kivy.app import App
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.textinput import TextInput
+from kivy.uix.checkbox import CheckBox
+from kivy.lang import Builder
+
 
 ROWS = ['Goc', 'COC', 'EEE', 'abs' , 'kju' , 'iop' , 'nmg', 'gty', 'jkio', 'dbkgcd' , 'udbcbjkb']
 
@@ -30,48 +33,20 @@ class Row(BoxLayout):
 class ScrollListItems(BoxLayout):
     pass
 
+class AddItemToShoppingList(BoxLayout):
+    def __init__(self):
+        print("AddItemToShoppingList")
+
+class MainWidget(GridLayout):
+    scrolllist_items = ObjectProperty()
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+
+    def open_add_item_screen(self):
+        AddItemToShoppingList()
+
 class ShoppingList(App):
     pass
-
 
 if __name__ == "__main__":
     ShoppingList().run()
-
-"""from kivy import Config
-from kivy.app import App
-from kivy.graphics import Rectangle, Color
-from kivy.metrics import dp
-from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.label import Label
-from kivy.uix.stacklayout import StackLayout
-
-Config.set("graphics", "width", "270")
-Config.set("graphics", "height", "480")
-
-
-class List(BoxLayout):
-    def __init__(self, **kwargs):
-        super(List, self).__init__(**kwargs)
-        for row in ROWS:
-            self.add_widget(Row(row))
-
-class Row(BoxLayout):
-    txt = StringProperty()
-    def __init__(self, row, **kwargs):
-        super(Row, self).__init__(**kwargs)
-        self.txt = row
-
-
-class MainWidget():
-    pass
-
-
-
-class ShoppingList(App):
-    pass
-
-if __name__ == "__main__":
-    ShoppingList().run()"""
